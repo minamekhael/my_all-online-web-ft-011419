2c1f0bbf9edcb49ceb4843e3e089f8fee412169a
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-15 18:55:45 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
-def my_all?([1, 2, 3])
+def my_all?(collection)
   i = 0
-  while i < 3
-    yield(1)
-    i += 1
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
 end
